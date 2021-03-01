bec753366036ccf97f74deaeaf32044d08cd7d0e
Ivan Notaros <nothke@gmail.com>
2021-03-01 16:05:57 +0100

    Removed json net folder and added new packages

---
+++ Assets/Scripts/ComplexObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComplexObject : MonoBehaviour, ISerializable, ISerializablePrefabLink
+{
+    public float value = 1;
+    public string prefabName => "box";
+
+    public class Data : ISerializableData
+    {
+        public string prefabName => "box";
+        public float value;
+        public int parentedId;
+    }
+
+    public ISerializableData data => new Data()
+    {
+        value = this.value,
+        parentedId = gameObject.GetInstanceID()
+    };
+}
---
+++ Assets/Scripts/Serializer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+using Newtonsoft.Json.Converters;
+
+public interface ISerializable
+{
+    ISerializableData data { get; }
+}
+
+public interface ISerializableLinksHandler
+{
+    void OnDeserializeHandleLinks();
+}
+
+public interface ISerializablePrefabLink
+{
+    string prefabName { get; }
+}
+
+public interface ISerializableData
+{
+    string prefabName { get; }
+}
+
+public class Serializer : MonoBehaviour
+{
+    public struct Loc
+    {
+        public Vector3 pos;
+        public Vector3 rot;
+        public Vector3 scl;
+
+        public Loc(Transform t)
+        {
+            pos = t.position;
+            rot = t.eulerAngles;
+            scl = t.localScale;
+        }
+    }
+
+    void Start()
+    {
+        var all = FindObjectsOfType<MonoBehaviour>();
+        for (int i = 0; i < all.Length; i++)
+        {
+            if (all[i] is ISerializable sobj)
+            {
+                Debug.Log("Found " + all[i].name);
+
+                var loc = new Loc(all[i].transform);
+
+                var str = JsonConvert.SerializeObject(loc, Formatting.Indented);
+
+                str += JsonConvert.SerializeObject(sobj.data, Formatting.Indented);
+
+
+                if (all[i] is ISerializablePrefabLink spref)
+                {
+                    JsonConvert.SerializeObject(spref.prefabName);
+                }
+
+                Debug.Log(str);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
